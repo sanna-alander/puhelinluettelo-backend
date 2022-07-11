71bfd7e1625fa0ab0ca8b9d01ad3ccfc10257928
salander <sanna.alander@nokia.com>
2022-07-11 15:59:39 +0300

    tehtävä 3.3

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
 
@@ -33,9 +34,20 @@ app.get('/info', (req, res) => {
         )
   })
   
-  app.get('/api/persons', (req, res) => {
+app.get('/api/persons', (req, res) => {
     res.json(persons)
   })
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
   
   const PORT = 3001
   app.listen(PORT, () => {
