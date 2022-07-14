766c6fab1f4359eb5c40281ce814a0ab159ac1df
salander <sanna.alander@nokia.com>
2022-07-14 16:12:09 +0300

    Finished ex. 3.6

--- index.js
+++ index.js
@@ -48,6 +48,40 @@ app.get('/api/persons/:id', (req, res) => {
         res.status(404).end()
     }
 })
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(p => p.id !== id)
+
+    res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+    const id = Math.random() * 10000
+    const body = req.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({ 
+          error: 'Name or number missing' 
+        })
+    }
+
+    if (persons.map(_.name).includes(body.name)) {
+        return response.status(400).json({ 
+            error: 'Name must be unique' 
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: id,
+    }
+
+    persons.concat(person)
+
+    res.json(person)
+})
   
   const PORT = 3001
   app.listen(PORT, () => {
